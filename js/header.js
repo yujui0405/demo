c31e2c1fea26e79dde97ef76f4069d28ca45e921
yujui0405 <d27752551@gmail.com>
2025-02-23 15:03:20 +0800

    登入改為燈箱效果

--- js/header.js
+++ js/header.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     if('scrollRestoration' in history){
         history.scrollRestoration = 'manual'
     }
+    $("#for_popup").load("../popLogin.html")
+    $(".popup_click").click(()=>{
+        $('#for_popup').toggleClass('for_popup_none')
+        $('.overlay').toggleClass('overlay_show')
+    })
+    $('.overlay').click(()=>{
+        $('.overlay').removeClass('overlay_show')
+        $('#for_popup').toggleClass('for_popup_none')
+    })
 })
