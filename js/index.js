5e0d6eb01a9d26d5503ff0ece8ed2a0f9f635d87
yujui0405 <d27752551@gmail.com>
2025-03-20 09:02:58 +0800

    修改header footer 在 Github上顯示問題

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-    $("#for_header").load("../header.html")
-    $("#for_footer").load("../footer.html")
+
+    console.log(window.location.protocol); // 协议，例如 "https:"
+    console.log(window.location.hostname); // 主机名，例如 "www.example.com"
+    console.log(window.location.port);     // 端口号，例如 "8080"（如果有）
+    console.log(window.location.pathname); // 路径，例如 "/page"
+    console.log(window.location.search);   // 查询字符串，例如 "?query=123"
+    console.log(window.location.hash);     // 锚点，例如 "#section"
+    // http://127.0.0.1:5500/index.html
+    let currentUrl = window.location.protocol + "//" + window.location.hostname 
+    if(window.location.port){
+        currentUrl += ":" + window.location.port
+    }
+    console.log(currentUrl)
+    // $("#for_header").load("../header.html")
+    // $("#for_footer").load("../fooeter.html")
+    $("#for_header").load(currentUrl + "/header.html")
+    $("#for_footer").load(currentUrl + "/footer.html")
 
     const swiper = new Swiper('.swiper', {
         // Optional parameters
