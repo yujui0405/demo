af1fb0d8de48c363f40de3a5cafa48dd0633d4a5
yujui0405 <d27752551@gmail.com>
2025-03-22 10:17:10 +0800

    修正header popup部分

--- js/header.js
+++ js/header.js
@@ -3,18 +3,22 @@ $(document).ready(function(){
     if('scrollRestoration' in history){
         history.scrollRestoration = 'manual'
     }
+
+    //popup
     $(".popup_click").click(()=>{
         let currentUrl = window.location.protocol + "//" + window.location.hostname 
         if(window.location.port){
             currentUrl += ":" + window.location.port
-            $("#for_popup").load("../popLogin.html")//要移到內部，才不會在網頁載入時，突然出現一點點又消失
+            $("#for_popup").load(currentUrl + "/popLogin.html")//要移到內部，才不會在網頁載入時，突然出現一點點又消失
         }else{
-            $("#for_popup").load("../demo/popLogin.html")//要移到內部，才不會在網頁載入時，突然出現一點點又消失
+            $("#for_popup").load(currentUrl + "/demo/popLogin.html")//要移到內部，才不會在網頁載入時，突然出現一點點又消失
         }
         
         $('#for_popup').toggleClass('for_popup_none')
         $('.overlay').toggleClass('overlay_show')
     })
+
+    //遮罩和點擊遮罩關掉popup
     $('.overlay').click(()=>{
         $('.overlay').removeClass('overlay_show')
         $('#for_popup').toggleClass('for_popup_none')
